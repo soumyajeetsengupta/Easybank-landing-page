297b51ee202bcbf06f76fdeb25134f50ed6e548b
soumyajeetsengupta <soumyajeetsengupta@gmail.com>
2023-12-05 00:13:20 +0530

    Animated Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello, World!");
+var hamburgureBTN = document.querySelector("#btn-hamburger");
+var header = document.querySelector(".header");
+var overlay = document.querySelector(".overlay");
+
+hamburgureBTN.addEventListener("click", function(){
+    header.classList.toggle('open');
+    overlay.classList.toggle('fade-in');
+    overlay.classList.toggle('fade-out', !overlay.classList.contains('fade-in'));
+});
